e107feeb9d577c92b58dba56980131a76f016933
ParamKataria <paramkataria2808@gmail.com>
2025-08-07 16:52:07 -0300

    latest

---
+++ Assets/MultiSet/Scripts/test.cs
@@ -0,0 +1,19 @@
+// using UnityEngine;
+
+// public class DisableMultisetPrefab : MonoBehaviour
+// {
+//     void Start()
+//     {
+//         GameObject sdkManager = GameObject.Find("MultisetSdkManager");
+
+//         if (sdkManager != null)
+//         {
+//             sdkManager.SetActive(false);
+//             Debug.Log("✅ MultisetSdkManager disabled at runtime.");
+//         }
+//         else
+//         {
+//             Debug.LogWarning("⚠️ MultisetSdkManager not found.");
+//         }
+//     }
+// }
